6fa61f774dedecf1e6cbfe26a36d9c21356d742e
jfk1998 <56309158+jfk1998@users.noreply.github.com>
2022-09-06 21:56:21 +0200

    added preafabs

--- project/Assets/Scenes/Scripts/Game/MapGenerator.cs
+++ project/Assets/Scenes/Scripts/Game/MapGenerator.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public GameObject groundPlanet;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
